1e1b6ac0fd1798af032f29b1a2d0a77bc29b0849
Mohnish <mohnish.a007@gmail.com>
2024-11-30 19:36:53 +0000

    Basic class implemented and debug

--- basicOOP/OOP1/oop1.cpp
+++ basicOOP/OOP1/oop1.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
+class Employee {
+        int id;
+public:
+    // Constructor for employee class
+    Employee(int i) {id = i;}
+    // display function to show the id of an employee
+    void printId() const { cout <<"Your employee ID is: "<< id << endl;} //const function
+};
+
+
 int main() {
-cout << "Hello World!" << endl;
+cout << "Hello World! creating employee \n" << endl;
+
+// Creating an employee object with id 100
+ cout << "creating employee with ID:100 " << endl;
+Employee emp(100);
+
+// Displaying the id of the employee using the printId function
+emp.printId();
 return 0;
 }
