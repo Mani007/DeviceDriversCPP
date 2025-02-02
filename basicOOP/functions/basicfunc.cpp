0da82d64330bb3f904720dab569fb2ccd5896c82
Mani007 <mohnish.a007@gmail.com>
2025-02-02 22:45:33 +0100

    basic sum function with function prototype with user input

--- basicOOP/functions/basicfunc.cpp
+++ basicOOP/functions/basicfunc.cpp
@@ -4,8 +4,15 @@ using namespace std;
 int sum(int a, int b);
 
 int main() {
+    int n1,n2;
     cout<<"Hello"<<endl;
-    cout<<"The sum of 3,4 is "<<sum(3,4)<<endl;
+    cout<<"Enter the num 1 as n1 "<< endl;
+    cin>>n1;
+    
+    cout<<"Enter the num 2 as n2 "<< endl;
+    cin>>n2;
+
+    cout<<"The sum of 3,4 is "<<sum(n1,n2)<<endl;
     return 0;
 }
 
