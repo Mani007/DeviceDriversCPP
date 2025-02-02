f4c15bbd6d209c59224731f5f0b1ecd08454acad
Mani007 <mohnish.a007@gmail.com>
2025-02-02 22:43:10 +0100

    basic sum function with function prototype

--- basicOOP/functions/basicfunc.cpp
+++ basicOOP/functions/basicfunc.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int sum(int a, int b){
-    return a+b;
-}
+int sum(int a, int b);
 
 int main() {
     cout<<"Hello"<<endl;
     cout<<"The sum of 3,4 is "<<sum(3,4)<<endl;
     return 0;
+}
+
+int sum(int a, int b){
+    return a+b;
 }
